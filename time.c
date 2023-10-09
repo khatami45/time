b77639c1e6376dc6889dcd25f9559bac418f1b20
khatami45 <avelivonia@gmail.com>
2023-10-09 08:27:06 -0400

    after go

--- time.c
+++ time.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
     int time;
+    int thour, tmin;
+    int hour, min, second;
+
     printf("Enter the time in second: ");
     scanf("%d", &time);
+
+    min = second / 60;
+    
+
 }
