08266cd4f4f3cc005fe117c2cb5cd7c20e5308ab
khatami45 <avelivonia@gmail.com>
2023-10-09 22:38:49 -0400

    compleated

--- time.c
+++ time.c
@@ -1,13 +1,23 @@
 #include<stdio.h>
+#include<time.h>
 int main(){
-    int time;
+    int times;
     int thour, tmin;
     int hour, min, second;
 
     printf("Enter the time in second: ");
-    scanf("%d", &time);
+    scanf("%d", &times);
+    
+   /*
+    times = time(NULL);
+    printf("current tine in second is: %d\n", times);
+   */
+    second = times % 60;
+    tmin = times / 60;
+    min = tmin % 60;
+    hour = tmin / 60;
 
-    min = second / 60;
+    printf(" %d : %d : %d ", hour, min, second);
     
 
 }
